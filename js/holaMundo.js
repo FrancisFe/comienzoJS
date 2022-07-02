606ac246f94675f33c58dc8662df1265de6f2c64
Francis <franciss.cs@hotmail.com>
2022-07-02 15:09:13 -0300

    Se realizaron operaciones basicas de js

---
+++ js/holaMundo.js
@@ -0,0 +1,32 @@
+//Comentario simple
+
+/*Comentario
+multi-linea */
+
+//mostrar mensaje en consola
+console.log("Hola mundo");
+
+//mostrar mensaje en html
+document.write("<p>Hola mundo</p>")
+
+//alerta
+//alert("Hola")
+
+//crear o declarar variables var - let - const
+
+let variableBuscada = "hola";
+document.write("<br>La palabra buscada es " + variableBuscada)
+
+//modificar el valor de una variable
+
+variableBuscada ="pagina del blog"
+document.write("<br>La palabra buscada es " + variableBuscada)
+
+const comision =17;
+document.write("<br>Constante comision: " + comision)
+
+let luz =true;
+
+let suma =10;
+console.log(--suma);
+console.log(suma -1);
