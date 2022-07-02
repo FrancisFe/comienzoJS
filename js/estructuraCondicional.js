ae9c4932e9054c25387b3c27ca09eb2692d72c3e
Francis <franciss.cs@hotmail.com>
2022-07-02 16:41:10 -0300

    Se agregaron algunos ejemplos de elseif y se agrego el archivos bucles.js

--- js/estructuraCondicional.js
+++ js/estructuraCondicional.js
@@ -1,9 +1,11 @@
-let primerNumero =parseInt(prompt("Ingrese el primer numero"));
+let primerNumero = parseInt(prompt("Ingrese el primer numero"));
 let segundoNumero = parseInt(prompt("Ingrese el segundo numero"));
 
-if(primerNumero>segundoNumero){
-    document.write("La variable primerNumero es mayor:  "+primerNumero)
+if (primerNumero > segundoNumero) {
+  document.write("La variable primerNumero es mayor:  " + primerNumero);
+} else if(segundoNumero>primerNumero){
+  document.write("La variable segundoNumero es mayor: " + segundoNumero);
 }
 else{
-    document.write("La variable segundoNumero es mayor: "+segundoNumero)
+document.write("Las variables son iguales");
 }
