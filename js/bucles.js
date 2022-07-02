4c17e5e17006ba22925c8757968c46813d4ec4af
Francis <franciss.cs@hotmail.com>
2022-07-02 20:42:09 -0300

    Se practicaron 3 tipos de bucles en la pagina bucles

---
+++ js/bucles.js
@@ -0,0 +1,16 @@
+let contador=1;
+/*
+while(contador <=100){
+    document.write("Elemento: "+ contador+ "<br>");
+    contador++;
+}*/
+
+/*
+do{
+    document.write("Elemento: "+contador +"<br>");
+    contador++
+}while(contador<=100)
+ */
+for(let i=1;i<100;i++){
+    document.write("Elemento: "+i+"<br>");
+}
